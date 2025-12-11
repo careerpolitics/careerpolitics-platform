4136858ea184247e0f8b5074b51d64b3bbb62af9
Murari Kumar <muraridevv@gmail.com>
2025-12-11 07:46:01 +0530

    FIX: Make migration script to single file

--- db/migrate/20251210073423_add_featured_to_job_posts.rb
+++
@@ -1,8 +0,0 @@
-class AddFeaturedToJobPosts < ActiveRecord::Migration[7.0]
-  disable_ddl_transaction!
-
-  def change
-    add_column :job_posts, :featured, :boolean, default: false, null: false
-    add_index :job_posts, :featured, algorithm: :concurrently
-  end
-end
