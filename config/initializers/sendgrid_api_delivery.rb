f2c59c86a540bcbf4add45b3ac5f5d6ed409de4e
Murari Kumar <muraridevv@gmail.com>
2026-02-06 08:46:15 +0530

    FEAT: Add sendgrid api details

--- config/initializers/sendgrid_api_delivery.rb
+++ config/initializers/sendgrid_api_delivery.rb
@@ -1,8 +1,10 @@
+# config/initializers/sendgrid_api_delivery.rb
 # frozen_string_literal: true
+
+require Rails.root.join("lib/sendgrid_api_delivery")
 
 ActionMailer::Base.add_delivery_method(
   :sendgrid_api,
   SendgridApiDelivery,
   api_key: ENV["SENDGRID_API_KEY"]
 )
-
