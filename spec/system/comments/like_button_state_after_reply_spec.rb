15e6b1ebf3998bc00ee2665f69e4831baf9b7f16
Murari Kumar <muraridevv@gmail.com>
2026-04-24 13:08:22 +0530

    Relax brittle single-comment presence check in reply spec

--- spec/system/comments/like_button_state_after_reply_spec.rb
+++ spec/system/comments/like_button_state_after_reply_spec.rb
@@ -21,7 +21,6 @@ RSpec.describe "Like button and tooltip after replying", js: true do
     find("form button[type='submit']", match: :first).click
 
     # Parent comment should no longer be in the `replying` state
-    expect(page).to have_css(".single-comment-node")
     expect(page).not_to have_css("#comment-node-#{parent_comment.id} .comment__details.replying")
 
     # Like button present and visible in the footer
